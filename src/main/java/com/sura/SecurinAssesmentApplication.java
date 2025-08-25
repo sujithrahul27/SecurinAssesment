8c133fcb09e3755237e27663332ab5f782acea91
sujitrahul <sujithrahul27@gmail.com>
2025-08-25 16:59:35 +0530

    Sucesfully parse the sample file

--- src/main/java/com/sura/SecurinAssesmentApplication.java
+++ src/main/java/com/sura/SecurinAssesmentApplication.java
@@ -1,13 +1,17 @@
-package com.sura.springmvc;
+package com.sura;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.sura.service.XmlParserService;
 
 @SpringBootApplication
 public class SecurinAssesmentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurinAssesmentApplication.class, args);
+		XmlParserService xmlservice = new XmlParserService();
+		xmlservice.parseXml();
 	}
 
 }
---
+++ src/main/java/com/sura/service/XmlParserService.java
@@ -0,0 +1,62 @@
+package com.sura.service;
+
+import java.io.File;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.springframework.stereotype.Service;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+@Service
+public class XmlParserService {
+	
+	public void parseXml(){
+		try {
+			File cpeDictnary = new File("src/main/java/sample.xml");
+			DocumentBuilderFactory documentFactory =  DocumentBuilderFactory.newInstance();
+			DocumentBuilder documentBuilder = documentFactory.newDocumentBuilder();
+			Document xmlDocument = documentBuilder.parse(cpeDictnary);
+			xmlDocument.getDocumentElement().normalize();
+			
+			NodeList nodes = xmlDocument.getElementsByTagName("CPE_Entry");
+			System.out.println(xmlDocument.getDocumentElement().getNodeName());
+			for(int i=0;i < nodes.getLength();i++) {
+				Node node = nodes.item(i);
+				Element element = (Element) node;
+				String title = element.getElementsByTagName("Title").item(0).getTextContent();
+                String cpe22Uri = element.getElementsByTagName("CPE_22_URI").item(0).getTextContent();
+                String cpe23Uri = element.getElementsByTagName("CPE_23_URI").item(0).getTextContent();
+                String deprecated22 = element.getElementsByTagName("CPE_22_Deprecated").item(0).getTextContent();
+                String deprecated23 = element.getElementsByTagName("CPE_23_Deprecated").item(0).getTextContent();
+                
+                NodeList references = element.getElementsByTagName("Link");
+                List<String> refers = new java.util.ArrayList<>();
+                for (int j = 0; j < references.getLength(); j++) {
+                    refers.add(references.item(j).getTextContent());
+                }
+                
+                System.out.println(title+cpe22Uri+cpe23Uri+deprecated22+deprecated23);
+                for(String s : refers) System.out.println(s);
+
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                LocalDate deprecated22Date = LocalDate.parse(deprecated22, formatter);
+                LocalDate deprecated23Date = LocalDate.parse(deprecated23, formatter);
+                System.out.println(deprecated22Date+" "+deprecated23Date);
+
+				
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
