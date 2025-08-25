1300f23c03678e101406408a124e7980d29768d8
sujitrahul <sujithrahul27@gmail.com>
2025-08-25 14:49:18 +0530

    Inital Commit

---
+++ src/main/java/com/sura/springmvc/SecurinAssesmentApplication.java
@@ -0,0 +1,13 @@
+package com.sura.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurinAssesmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurinAssesmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sura/springmvc/SecurinAssesmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sura.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurinAssesmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
